ebd4afc20d61a92cfffcf8ae2b13492098c54026
wangyang199107 <694866518@qq.com>
2020-07-17 11:17:27 +0800

    Add files via upload

---
+++ Commands/DelegateCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CrazyElephant.Commands
+{
+    class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        
+        public  Action<object> ExecuteAction { get; set; }
+        public Func<object,bool> CanExecuteFunc { get; set; }
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.CanExecuteFunc == null)
+            {
+                return true;
+            }
+
+           return this.CanExecuteFunc(parameter);
+
+        }
+
+        public void Execute(object parameter)
+        {
+            if (this.ExecuteAction == null)
+            {
+                return;
+            }
+
+            this.ExecuteAction(parameter);
+        }
+    }
+}
